b7f2da42e3a1b808215eb56418496eee99da762d
ANGELACAMU <anyo333@gmail.com>
2024-11-07 17:47:16 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,2 +1,7 @@
-console.log(">> Ready :)");
+const i=document.querySelector(".js-button-search"),a=document.querySelector(".js-input"),o=document.querySelector(".js-section");function r(s){s.preventDefault(),console.log("ha hecho click");const n=a.value;console.log(n),fetch(`https://api.jikan.moe/v4/anime?q=${n}`).then(e=>e.json()).then(e=>{const t=e.data;console.log(t),o.innerHTML="";for(const c of t)o.innerHTML+=`
+                <section class="sectionSearch">
+                    <h5>${c.title}</h5>
+                    <img src="${c.images.jpg.image_url}" alt="imagen-anime">
+                </section>
+            `})}i.addEventListener("click",r);
 //# sourceMappingURL=main.js.map
