286c528d3d0a90c4d9bb5bc9379a2eed171a4b44
ANGELACAMU <anyo333@gmail.com>
2024-11-07 12:12:07 +0100

    fetch added still testing in mainjs dev

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,43 @@
 'use strict';
+
+/* Pintar las series que me da el servidor
+Seleccionar los elementos del HTML: lista
+Cuando la usuaria hace click en el boton buscar:
+  Recoger el valor del input
+  -hacer la peticion al servidor
+    - acceder/guardar los datos que necesita
+    - pintar los datos en el html
+*/
+
 const buttonSearch = document.querySelector(".js-button-search");
 const inputSearch = document.querySelector(".js-input");
 
 const sectionSearch = document.querySelector(".js-section");
 
-function handleClick (ev){
+function handleClick(ev) {
     ev.preventDefault();
-console.log ("ha hecho click");
+    console.log("ha hecho click");
+    const inputText = inputSearch.value;
+    console.log(inputText);
+    fetch(`https://api.jikan.moe/v4/anime?q=${inputText}`)
+        .then(res => res.json())
+        .then(info => {
+            const series = info.data;
+            console.log(series);
+            for (const serie of series) {
+                sectionSearch.innerHTML = `
+                <section class="sectionSearch">
+                    <h5>${series}</h5>
+                </section>
+            `
+            }
+
+        })
+
 }
+
+
+
+
 
 buttonSearch.addEventListener("click", handleClick);
