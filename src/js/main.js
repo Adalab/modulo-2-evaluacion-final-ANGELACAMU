de385ec61bd6da1723da48bc90e97a7a97d8b6da
ANGELACAMU <anyo333@gmail.com>
2024-11-07 13:33:49 +0100

    adding human steps

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,28 @@
 'use strict';
 
-/* Pintar las series que me da el servidor
+/* 2.BÚSQUEDA
+Pintar las series que me da el servidor
 Seleccionar los elementos del HTML: lista
 Cuando la usuaria hace click en el boton buscar:
   Recoger el valor del input
   -hacer la peticion al servidor
     - acceder/guardar los datos que necesita
     - pintar los datos en el html
+
+   3.FAVORITOS
+Cuando la usuaria haga click sobre la serie en concreto:
+  - averiguar que serie ha seleccionado la usuaria
+  - almacenar la informacion de la serie favorita
+  - intercambiar el color de fondo y el de fuente
+  - mostrar un listado en la parte izquierda de la pantalla, debajo del         formulario de búsqueda, con las series favoritas
+  
+
+  4.ALMACENAMIENTO
+  almacenar el listado de favoritos en el localStorage
+
 */
+
+
 
 const buttonSearch = document.querySelector(".js-button-search");
 const inputSearch = document.querySelector(".js-input");
