6d9755c1b6c56ab19704794169fcfc50ccd58c09
ANGELACAMU <anyo333@gmail.com>
2024-11-07 10:11:14 +0100

    moving main and scss to dev

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
 'use strict';
+const buttonSearch = document.querySelector(".js-button-search");
+const inputSearch = document.querySelector(".js-input");
+
+const sectionSearch = document.querySelector(".js-section");
+
+function handleClick (ev){
+    ev.preventDefault();
+console.log ("ha hecho click");
+}
+
+buttonSearch.addEventListener("click", handleClick);
