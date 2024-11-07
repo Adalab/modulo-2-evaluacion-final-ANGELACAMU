f3997646bf11a119cb3d7b1f9a38c074172fc3d5
ANGELACAMU <anyo333@gmail.com>
2024-11-07 17:40:25 +0100

    save changes in readme

--- src/js/main.js
+++ src/js/main.js
@@ -24,11 +24,8 @@ Cuando la usuaria haga click sobre la serie en concreto:
 
 */
 
-
-
 const buttonSearch = document.querySelector(".js-button-search");
 const inputSearch = document.querySelector(".js-input");
-
 const sectionSearch = document.querySelector(".js-section");
 
 function handleClick(ev) {
